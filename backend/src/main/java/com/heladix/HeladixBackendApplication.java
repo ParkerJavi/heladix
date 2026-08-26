234daaed71085a800f519434b196528c45a7c5ed
ParkerJavi <parkerjavi@gmail.com>
2026-08-26 11:38:41 -0600

    feat: initialize spring boot backend

---
+++ backend/src/main/java/com/heladix/HeladixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.heladix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeladixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeladixBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/heladix/HeladixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heladix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeladixBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
